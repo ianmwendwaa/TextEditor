b7cd4d0e86433643b20af64c818b976dd19d6cb6
ianmwendwaa <williamsian845@gmail.com>
2025-07-26 01:17:20 +0300

    modified files and other variables

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/deploy_TextEditor_8a779e266d.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/QtProjects/TextEditor/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/TextEditor-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/QtProjects/TextEditor/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/TextEditor.exe
-    GENERATE_QT_CONF
-)
