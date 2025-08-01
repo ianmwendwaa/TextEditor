4aed07ee974103e1a07f93124e3f1e3d0b144f62
ianmwendwaa <williamsian845@gmail.com>
2025-08-01 23:30:51 +0300

    Fixed bugged out pointers

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TextEditor_FILE C:/QtProjects/TextEditor/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/TextEditor.exe)
-set(__QT_DEPLOY_TARGET_TextEditor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TextEditor_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
